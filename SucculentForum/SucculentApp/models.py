c856462eab4fd8304a342a0b9c17a7d28d71bd9a
Ben <benjamin.schwab.nonspam@gmail.com>
2019-01-28 19:24:31 -0800

    topic, thread models

--- SucculentForum/SucculentApp/models.py
+++ SucculentForum/SucculentApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    Title = models.CharField(max_length=50)
+    DateUpdated = models.DateField()
+    ThreadCount = models.IntegerField()
+
+class Thread(models.Model):
+    Title = models.CharField(max_length=50)
+    DateUpdated = models.DateField()
+    Body = models.CharField(max_length=50)
+    UserId = models.ForeignKey(User, on_delete=models.CASCADE)
+    ViewCount = models.IntegerField()
+    TopicId = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    DateStarted = models.DateField()
+    PostCount = models.IntegerField()
+
