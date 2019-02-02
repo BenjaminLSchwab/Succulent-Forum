d29360bcb16235c2aca7dc49c2670f62e5c141b3
Ben <benjamin.schwab.nonspam@gmail.com>
2019-02-02 14:06:25 -0800

    Here are the rest of the models, along with another migration

--- SucculentForum/SucculentApp/models.py
+++ SucculentForum/SucculentApp/models.py
@@ -10,10 +10,38 @@ class Topic(models.Model):
 class Thread(models.Model):
     Title = models.CharField(max_length=50)
     DateUpdated = models.DateField()
-    Body = models.CharField(max_length=50)
+    Body = models.CharField(max_length=10000)
     UserId = models.ForeignKey(User, on_delete=models.CASCADE)
     ViewCount = models.IntegerField()
     TopicId = models.ForeignKey(Topic, on_delete=models.CASCADE)
     DateStarted = models.DateField()
     PostCount = models.IntegerField()
 
+class Poll(models.Model):
+    Title = models.CharField(max_length=50)
+    StartDate = models.DateField()
+    EndDate = models.DateField()
+
+class Post(models.Model):
+    PollId = models.ForeignKey(Poll, null=True, on_delete=models.CASCADE)
+    Body = models.CharField(max_length=10000)
+
+
+class Choice(models.Model):
+    PollId = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    VoteCount = models.IntegerField()
+
+class Vote(models.Model):
+    PollId = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    UserId = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Message(models.Model):
+    Title = models.CharField(max_length = 50)
+    Body = models.CharField(max_length = 10000)
+    DateSent = models.DateField()
+    DateRead = models.DateField(null = True)
+    SenderId = models.ForeignKey(User, null= True, on_delete=models.CASCADE)
+
+class MessageRecipientLink(models.Model):
+    MesageId = models.ForeignKey(Message, on_delete=models.CASCADE)
+    UserId = models.ForeignKey(User, on_delete=models.CASCADE)
