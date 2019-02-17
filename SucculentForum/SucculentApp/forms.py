64a5c773641ad3476311aa7c2756ad343187b48a
SeanGoldsmith <goldsmith.sean90@gmail.com>
2019-02-17 12:34:15 -0800

    The thread Creation is working.  The code is a mess.  Refactoring next.

--- SucculentForum/SucculentApp/forms.py
+++ SucculentForum/SucculentApp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from SucculentApp.models import Topic
 
 class SignUpForm(UserCreationForm):
     
@@ -8,3 +9,16 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'password1', 'password2', )
+
+class ThreadForm(forms.Form):
+    title = forms.CharField(max_length=100)
+    DateUpdated = forms.DateField(widget = forms.HiddenInput(),required=False)
+    Body = forms.CharField()
+    UserId = forms.ModelChoiceField(queryset=User.objects.all(),widget=forms.HiddenInput(),required=False)
+    ViewCount = forms.IntegerField(widget=forms.HiddenInput(),required=False)
+    TopicId = forms.ModelChoiceField(queryset=Topic.objects.all(),widget=forms.HiddenInput(),required=False)
+    DateStarted = forms.DateField(widget = forms.HiddenInput(),required=False)
+    PostCount = forms.IntegerField(widget=forms.HiddenInput(),required=False)
+    
+
+    
